b1029b38210794555c0ebec616b6b9c89736fd25
Casey Hild <44584719+caseyhild@users.noreply.github.com>
2024-02-16 11:26:20 -0800

    Initial Commit

---
+++ starter/MyAlgorithm.java
@@ -0,0 +1,17 @@
+/**
+ * This class contains an algorithm utilizing a stack or queue. 
+ */
+public class MyAlgorithm {
+    /**
+     * Finds the average difference between consecutive elements in the
+     * monotonically increasing subsequence starting from the first element in
+     * the array
+     * 
+     * @param arr the array containing the monotonic subsequence
+     * @throws NullPointerException if the specified array is null
+     * @return the average difference between elements in the subsequence
+     */
+    public static double avgDiffMonotonicIncreasing(int[] arr) {
+        return 0;
+    }
+}
